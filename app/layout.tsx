f19a6315417ac1bcbf6147e836015aab50070c8f
Hemanta Rana <magarhemanta145@gmail.com>
2026-08-24 15:17:15 +0545

    added auth using clerk

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,28 +3,31 @@ import { Poppins } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/common/Header";
 import Footer from "@/components/common/Footer";
+import { ClerkProvider } from "@clerk/nextjs";
 
 const poppins = Poppins({
-  subsets: ['latin'],
-  weight: ['400']
+  subsets: ["latin"],
+  weight: ["400"],
 });
 
 export const metadata: Metadata = {
   title: "I buit this - Share your project online",
-  description: "Share your project online and get feedback from the community. Discover new projects and connect with creators. ",
+  description:
+    "Share your project online and get feedback from the community. Discover new projects and connect with creators. ",
 };
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html
-      lang="en"
-      className={`${poppins.className} h-full antialiased`}
-    >
+    <ClerkProvider>
+
+   
+    <html lang="en" className={`${poppins.className} h-full antialiased`}>
       <body className="min-h-full flex flex-col">
         <Header />
         {children}
-        <Footer/>
-        </body>
+        <Footer />
+      </body>
     </html>
+     </ClerkProvider>
   );
 }
--- components/common/Header.tsx
+++ components/common/Header.tsx
@@ -1,5 +1,13 @@
 import Link from "next/link";
 import {
+  ClerkProvider,
+  Show,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
+import { Geist, Geist_Mono } from "next/font/google";
+import {
   ArrowRight,
   ChevronsRight,
   CompassIcon,
@@ -13,19 +21,21 @@ import { Button } from "@/components/ui/button";
 const Logo = () => {
   return (
     <Link href={"/"} className="flex items-center group">
-      <div className="size-8 rounded-full flex items-center justify-center bg-primary ">
-        <Squirrel className="size-4 text-primary-foreground" />
+      <div className=" flex gap-2">
+        <div className="size-8 rounded-full flex items-center justify-center bg-primary ">
+          <Squirrel className="size-4 text-primary-foreground" />
+        </div>
+        <span className="text-xl font-bold text-red-600">i</span>
+      
       </div>
-      <span className="text-xl font-bold text-red-600">i</span>
       <span className="text-black font-extrabold">B </span>
-      <span className="text-xl text-red-600 font-bold">uilt</span>
-
-      <span className="text-xl font-bold">This</span>
+        <span className="text-xl text-red-600 font-bold">uilt</span>
+        <span className="text-xl font-bold">This</span>
     </Link>
   );
 };
 export default function Header() {
-  const isSignedIn = true;
+  const isSignedIn = false;
   return (
     <header className="sticky top-0 z-50 border-b bg-background/95 backdrop-blur supports-backdrop-filter:bg-background/60">
       <div className="wrapper px-12">
@@ -46,30 +56,27 @@ export default function Header() {
               <CompassIcon className=" size-4" />
               <span>Explore</span>
             </Link>
-             </nav>
-            <div className="flex items-center gap-3">
-              {isSignedIn ? (
-                <>
-                
+          </nav>
+          <div className="flex items-center gap-3">
+            <Show when="signed-out">
+              <SignInButton />
+              <SignUpButton>
+                <button className=" bg-black rounded-full text-white text-sm px-4 py-1 cursor-pointer">
+                  Sign Up
+                </button>
+              </SignUpButton>
+            </Show>
+            <Show when="signed-in">
+              <>
                 <Button className=" rounded-full px-2">
                   <Link href={"/submit"}></Link>
                   <span>Submit Project</span>
                   <ChevronsRight className=" size-4" />
                 </Button>
-                <UserIcon className="size-4 " />
-
-                
-                </>
-              ) : (
-                <>
-                  <Button variant="ghost" className="rounded-full">
-                    Sign In
-                  </Button>
-                  <Button className="rounded-full">Sign Up</Button>
-                </>
-              )}
-            </div>
-         
+              </>
+              <UserButton />
+            </Show>
+          </div>
         </div>
       </div>
     </header>
